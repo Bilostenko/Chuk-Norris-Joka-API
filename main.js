3b462eff344001701512357056d91d8c38b346c9
Bilostenko <bilostenkod@gmail.com>
2023-03-02 15:30:23 +0200

    add cetegoires

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const randomBtn = document.querySelector('#option1');
 const searchInput = document.querySelector('#search-data-input');
 const searchBtn = document.querySelector('#option3');
@@ -14,30 +13,23 @@ let randomJoke;
 async function getDataFromAPIRandom() {
   const response = await fetch('https://api.chucknorris.io/jokes/random');
   randomJoke = await response.json();
+  console.log(randomJoke)
   return randomJoke;
 }
-getDataFromAPIRandom().then(randomJoke => {
-  console.log(randomJoke);
-}).catch(error => {
-  console.error(error);
-});
 
-/* click on Random button for insert joke */
-/* randomBtn.addEventListener('click', () => {
-  
-  getDataFromAPIRandom().then(randomJoke => {
-    const joke = document.querySelector('#joke');
-    joke.innerHTML = randomJoke.value;
-  }).catch(error => {
-    console.error(error);
-  });
-}) */
+/* sending request to API for CATEGORY joke */
+let categoryJoke;
+async function getDataFromAPICategory() {
+  const response = await fetch('https://api.chucknorris.io/jokes/categories');
+  categoryJoke = await response.json();
+  return categoryJoke;
+}
 
 button.addEventListener('click', () => {
-  const options = document.getElementsByName("options");
-    for (let i = 0; i < options.length; i++) {
-    if (options[i].checked == true && options[i].value == 'option1') {
-      console.log(options[i].checked );
+  const allRadioBtns = document.getElementsByName("options");
+  for (let i = 0; i < allRadioBtns.length; i++) {
+    /* --------------------------------------------RANDOM----------------------- */
+    if (allRadioBtns[i].checked == true && allRadioBtns[i].value == 'option1') {
       getDataFromAPIRandom().then(randomJoke => {
         const joke = document.querySelector('#joke');
         joke.innerHTML = randomJoke.value;
@@ -45,12 +37,32 @@ button.addEventListener('click', () => {
         console.error(error);
       });
       break;
+     /* ------------------------------ CATEGORY--------------------------------------- */
+    } else if (allRadioBtns[i].checked == true && allRadioBtns[i].value == 'option2') {
+      getDataFromAPICategory().then(categories => {
+        const categoryList = document.createElement('div');
+        categoryList.classList.add('category-inputs');
+        categories.forEach(category => {
+          const categoryInput = document.createElement('input');
+          categoryInput.type = 'radio';
+          categoryInput.name = 'category';
+          categoryInput.value = category;
+          categoryInput.id = `category-${category}`;
+          const label = document.createElement('label');
+          label.textContent = category;
+          label.htmlFor = `category-${category}`;
+          label.classList.add('category-label');
+          categoryList.appendChild(categoryInput);
+          categoryList.appendChild(label);
+        });
+        const joke = document.querySelector('#joke');
+        joke.innerHTML = '';
+        joke.appendChild(categoryList);
+      }).catch(error => {
+        console.error(error);
+      });
+      break;
     }
   }
 
-  })
-
-
-
-
-
+})
